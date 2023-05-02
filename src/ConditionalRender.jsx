47a9950cd10b7cd41b634ebd90b9adf380f1a8b2
Kaian Samuel <kaiansamuelster@gmail.com>
2023-05-02 19:57:31 -0300

    If Else no JSX

--- src/ConditionalRender.jsx
+++ src/ConditionalRender.jsx
@@ -2,10 +2,21 @@ import { useState } from 'react'
 
 const ConditionalRender = () => {
   const [x] = useState(true)
+  const [name] = useState('João')
+
   return (
     <div>
       <h1>Isso será exibido?</h1>  
       { x && <p>Se for true, sim!</p> }
+      {name === 'João' ? (
+        <div>
+          <p>O nome é João</p>
+        </div>
+      ) : (
+        <div>
+          <p>Nome não encontrado!</p>
+        </div>
+      )}
     </div>
   )
 }
