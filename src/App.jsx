491c4072b46e85ce4d7f3e02df41fd008ffbb7d5
Kaian Samuel <kaiansamuelster@gmail.com>
2023-05-02 21:35:46 -0300

    reaproveitamento de componentes, renderização de lista de componentes, prop children, função como prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
 import React from 'react'
 import ConditionalRender from './ConditionalRender'
 import CarDetails from './CarDetails'
+import Container from './Container'
+import FunctionProps from './FunctionProps'
+
+function showMessage(){
+  console.log('Hello World!')
+}
 
 const App = () => { 
+  const  cars = [
+    { id: 1, marca: 'Ferrari', color: 'Amarelo', newCar: true, km: 0 },
+    { id: 2, marca: 'Kia', color: 'Branco', newCar: false, km: 3144 },
+    { id: 3, marca: 'Renault', color: 'Azul', newCar: false, km: 9234 },
+  ]
   return (
+   
     <div>
       <ConditionalRender />
       <CarDetails marca='Audi' km= {48} cor='Verde' />
+      <CarDetails marca= 'Vm' km= {0} cor='Vermelho' />
+      {cars.map((car) => (
+        <CarDetails 
+          key={car.id}
+          brand={car.marca}
+          color={car.color}
+          km={car.km}
+          newCar={car.newCar}
+        />
+      ))}
+      <Container>
+        <p>Este é um paragrafo children</p>
+      </Container>
+      <FunctionProps myFunction={showMessage} />
     </div>
-   
   )
 }
 
-export default App
+export default App;
---
+++ src/FunctionProps.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FunctionProps = ({ myFunction }) => {
+  return (
+    <div>
+      <button onClick={myFunction}>Clique Aqui!</button>
+    </div>
+  )
+}
+
+export default FunctionProps
